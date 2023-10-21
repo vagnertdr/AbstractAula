13f33d38213104f08799869edf2d1bf9deaf41e1
Vagner Rocha <vagnerrochab2@gmail.com>
2023-10-21 03:05:47 -0300

    Initial commit

--- src/MSNMessenger.java
+++ src/MSNMessenger.java
@@ -1,3 +1,12 @@
 public class MSNMessenger extends ServicoMensagemInstantanea {
 
+    @Override
+    public void enviarMensagem() {
+        System.out.println("Enviando Mensangem pelo Msn Messenger");
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo Msn Messenger");
+    }
 }
